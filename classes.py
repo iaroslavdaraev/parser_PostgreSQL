5236b41e8b2c375ea4380126f512a61fad42c5a6
Iaroslav <yaroslav.da@mail.ru>
2024-02-27 23:54:32 +0300

    add dbmanager

--- classes.py
+++ classes.py
@@ -21,20 +21,58 @@ class DBManager:
 
     def get_companies_and_vacancies_count(cur):
         """Получает список всех компаний и количество вакансий у каждой компании"""
-        pass
+        cur.execute(
+            """
+        SELECT company_name, COUNT(*) AS vacancies_quantity
+        FROM vacancies
+        GROUP BY company_name
+        ORDER BY vacancies_quantity DESC"""
+        )
+        rows = cur.fetchall()
+        data_info = {}
+        for row in rows:
+            data_info[row[0]] = row[1]
+        return data_info
 
-    def get_all_vacancies(self):
+    def get_all_vacancies(cur):
         """Получает список всех вакансий с указанием названия компании, названия вакансии и зарплаты и ссылки на вакансию"""
-        pass
+        cur.execute("SELECT company_name, vacancy_name, salary_to, vacancy_url FROM vacancies")
+        rows = cur.fetchall()
+        data_info = []
+        for row in rows:
+            data_info.append(list(row))
+        return data_info
 
-    def get_avg_salary(self):
+    def get_avg_salary(cur):
         """Получает среднюю зарплату по вакансиям"""
-        pass
+        cur.execute(f"SELECT AVG(salary_to) FROM vacancies")
+        avg_salary = cur.fetchall()
+        return int(avg_salary[0][0])
 
-    def get_vacancies_with_higher_salary(self):
+    def get_vacancies_with_higher_salary(cur):
         """Получает список всех вакансий, у которых зарплата выше средней по всем вакансиям"""
-        pass
+        cur.execute(
+            f"""
+                SELECT company_name, vacancy_name, salary_to, vacancy_url FROM vacancies
+                WHERE salary_to > (SELECT AVG(salary_to) FROM vacancies)
+            """
+        )
+        rows = cur.fetchall()
+        data_info = []
+        for row in rows:
+            data_info.append(list(row))
+        return data_info
 
-    def get_vacancies_with_keyword(self):
+    def get_vacancies_with_keyword(cur):
         """Получает список всех вакансий, в названии которых содержатся переданные в метод слова"""
-        pass
+        cur.execute(
+            f"""
+                SELECT company_name, vacancy_name, salary_to, vacancy_url 
+                FROM vacancies
+                WHERE vacancy_name LIKE '%{word}%'"""
+        )
+        rows = cur.fetchall()
+        data_info = []
+        for row in rows:
+            data_info.append(list(row))
+        return data_info
