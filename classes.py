902ca85921191222c84455a6852f0eca096a0d9f
Iaroslav <yaroslav.da@mail.ru>
2024-02-27 23:35:23 +0300

    classes

--- classes.py
+++ classes.py
@@ -1,3 +1,40 @@
+import requests
+
+
 class HeadHunterAPI:
     """Класс для работы с API HeadHunter"""
-    pass
+
+    def __init__(self):
+        self.url = 'https://api.hh.ru/vacancies?employer_id='
+
+    def get_companies_vacancies(self, company_id: int):
+        """Полкчает данные через API"""
+        try:
+            company_vacancy = requests.get(f'{self.url}{company_id}', {'per_page': 50}).json()
+            return company_vacancy
+        except Exception as error:
+            raise Exception(f'Ошибка {error}')
+
+
+class DBManager:
+    """Класс для работы с базой данных PostgreSQL"""
+
+    def get_companies_and_vacancies_count(cur):
+        """Получает список всех компаний и количество вакансий у каждой компании"""
+        pass
+
+    def get_all_vacancies(self):
+        """Получает список всех вакансий с указанием названия компании, названия вакансии и зарплаты и ссылки на вакансию"""
+        pass
+
+    def get_avg_salary(self):
+        """Получает среднюю зарплату по вакансиям"""
+        pass
+
+    def get_vacancies_with_higher_salary(self):
+        """Получает список всех вакансий, у которых зарплата выше средней по всем вакансиям"""
+        pass
+
+    def get_vacancies_with_keyword(self):
+        """Получает список всех вакансий, в названии которых содержатся переданные в метод слова"""
+        pass
--- main.py
+++ main.py
@@ -1,2 +1,12 @@
+from classes import HeadHunterAPI
+from config import config
+
+vacancies = HeadHunterAPI()
+
+db_name = 'vacancies'
+
+paramas = config
+
+
 if __name__ == "__main__":
     pass
